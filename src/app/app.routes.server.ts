42546f35a02ea7cba57d44fe39cb5ff8c90e58ac
alhousen <traorealhoussen@gmail.com>
2026-01-22 23:55:43 +0000

    first commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,23 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function getCourseIds(): Promise<string[]> {
+  const dbPath = path.join(process.cwd(), 'db.json');
+  const dbData = await fs.promises.readFile(dbPath, 'utf8');
+  const db = JSON.parse(dbData);
+  return db.courses.map((course: any) => course.id.toString());
+}
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'courses/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const ids = await getCourseIds();
+      return ids.map(id => ({ id }));
+    },
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
