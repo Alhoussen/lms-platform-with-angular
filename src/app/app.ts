a3a0e7239748a0daea1283433d9b4b097213d3aa
alhousen <traorealhoussen@gmail.com>
2026-01-23 11:26:54 +0000

    Final version for deploy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, RouterLink, RouterLinkActive, HttpClientModule],
+  imports: [RouterOutlet, RouterModule, RouterLink, RouterLinkActive, HttpClientModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('lms-platform');
+  protected readonly authService = inject(AuthService);
 }
